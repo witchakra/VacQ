804b5bcbddf0719c7dbc19e7d8553aa3ba97107f
witchakra <131113499+witchakra@users.noreply.github.com>
2024-02-27 10:13:30 +0700

    Authentication

--- routes/hospital.js
+++ routes/hospital.js
@@ -4,8 +4,10 @@ const {getHospitals,getHospital,createHospital,updateHospital,deleteHospital} = 
 
 const router = express.Router();
 
-router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+const {protect,authorize} = require('../middleware/auth');
+
+router.route('/').get(getHospitals).post(protect,authorize('admin'),createHospital);
+router.route('/:id').get(getHospital).put(protect,authorize('admin'),updateHospital).delete(protect,authorize('admin'),deleteHospital);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
@@ -7,12 +8,16 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 const hospitals = require('./routes/hospital');
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use('/api/V1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT || 5001;
 
